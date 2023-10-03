129d60dde4b0eb14a87be553946ad88adcbb9f6e
Alex <hulsy89@gmail.com>
2023-10-03 12:48:12 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Alex Hulford on 2023-10-03.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
